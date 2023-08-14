2eb57aa7da3fe978d620399c127dc11ebeb9559d
Fabiano Rong <fabianorong@gmail.com>
2023-08-14 14:42:48 -0300

    nova alteração

--- 3/3.py
+++ 3/3.py
@@ -4,8 +4,21 @@ import pandas as pd
 with open("3\dados_compras.json") as f:
     data = json.load(f)
 
+df = pd.DataFrame(
+    columns=["Login", "Idade", "Sexo", "Item ID", "Nome do item", "Valor"]
+)
+
 for i in range(0, len(data)):
-    print(data[i])
+    currentitem = data[i]
+    df.loc[i] = [
+        data[i]["Login"],
+        data[i]["Idade"],
+        data[i]["Sexo"],
+        data[i]["Item ID"],
+        data[i]["Nome do Item"],
+        data[i]["Valor"],
+    ]
 
 # Identifique a quantidade total de compras realizadas.
+print(df)
 print(f"quantidade total de compras realizadas: {len(data)}")
