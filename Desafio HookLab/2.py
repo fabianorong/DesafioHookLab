61f5b33d029329359f3a5aae36e28b700f0d5a2e
Fabiano Rong <fabianorong@gmail.com>
2023-08-14 14:02:46 -0300

    exercicio resolvido

--- Desafio HookLab/2.py
+++ Desafio HookLab/2.py
@@ -4,12 +4,19 @@ filmes = [
     {"titulo": "Interestelar", "ano": 2014, "avaliacao": 8.6},
 ]
 
-# media das avaliaçoes
+# Média das avaliações
 
 avaliacoes = [filme["avaliacao"] for filme in filmes]
 soma = sum(avaliacoes)
 media = soma / 3
 print("A média das avaliaçoes dos filmes é:", (media))
 
-# titulo do filme com maior avaliação
-# ano de lançaento do filme com menor avaliação
+# Titulo do filme com maior avaliação
+filme_maior_avaliacao = max(filmes, key=lambda x: x["avaliacao"])
+print(f"O título do filme com a maior avaliação é {filme_maior_avaliacao['titulo']}")
+
+# Ano de lançamento do filme com menor avaliação
+filme_menor_avaliacao = min(filmes, key=lambda x: x["avaliacao"])
+print(
+    f"O ano de lançamento do filme com menor avaliação é {filme_menor_avaliacao['ano']}"
+)
