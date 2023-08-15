de95d91119f489b5da7df78acff24738f9e518a0
Fabiano Rong <fabianorong@gmail.com>
2023-08-15 10:39:06 -0300

    atualizado a tabela para conter mais infos

--- 3/3-teste.py
+++ 3/3-teste.py
@@ -7,9 +7,23 @@ with open("3\dados_compras.json") as f:
 tabela = pd.DataFrame(data)
 tabela_agrupada = (
     tabela.groupby("Login")
-    .agg({"Idade": "unique", "Sexo": "unique", "Valor": "sum"})
+    .agg(
+        {
+            "Idade": "unique",
+            "Sexo": "unique",
+            "Item ID": "unique",
+            "Nome do Item": "unique",
+            "Valor": "sum",
+        }
+    )
     .reset_index()
 )
 print(tabela_agrupada)
 
+# Identifique a quantidade total de compras realizadas.
 print(f"Quantidade total de compras realizadas: {len(tabela_agrupada)}")
+
+# Calcule a média, o valor mínimo e máximo gasto por compra.
+soma = tabela_agrupada["Valor"].sum()
+media = soma / (len(tabela_agrupada))
+print(f"Valor medio gasto por compra:{media}")
