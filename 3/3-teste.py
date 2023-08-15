3da5c9798b3b70fc22ec4336bcc9a603098a8e53
Fabiano Rong <fabianorong@gmail.com>
2023-08-15 11:06:46 -0300

    adicionado uma parte da analise de compra

--- 3/3-teste.py
+++ 3/3-teste.py
@@ -26,4 +26,10 @@ print(f"Quantidade total de compras realizadas: {len(tabela_agrupada)}")
 # Calcule a média, o valor mínimo e máximo gasto por compra.
 soma = tabela_agrupada["Valor"].sum()
 media = soma / (len(tabela_agrupada))
-print(f"Valor medio gasto por compra:{media}")
+print(f"Valor medio gasto por compra: {media}")
+
+max_valor = tabela_agrupada["Valor"].max()
+print(f"Valor maximo gasto por compra: {max_valor}")
+
+min_valor = tabela_agrupada["Valor"].min()
+print(f"Valor minimo gasto por compra: {min_valor}")
