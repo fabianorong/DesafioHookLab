242fbbba384470eb072397fc3e704a38dbde708b
Fabiano Rong <fabianorong@gmail.com>
2023-08-15 11:38:13 -0300

    adicionado analise de compra completo

--- 3/3-teste.py
+++ 3/3-teste.py
@@ -33,3 +33,25 @@ print(f"Valor maximo gasto por compra: {max_valor}")
 
 min_valor = tabela_agrupada["Valor"].min()
 print(f"Valor minimo gasto por compra: {min_valor}")
+
+# Determine o produto mais caro e o produto mais barato.
+
+df = tabela.explode("Valor").explode("Item ID").explode("Nome do Item")
+max_valor = df["Valor"].max()
+min_valor = df["Valor"].min()
+
+produto_mais_caro = df.loc[
+    df["Valor"] == max_valor, ["Valor", "Item ID", "Nome do Item"]
+]
+
+produto_mais_barato = df.loc[
+    df["Valor"] == min_valor, ["Valor", "Item ID", "Nome do Item"]
+]
+
+df2 = produto_mais_caro.drop_duplicates("Item ID", keep="first")
+df3 = produto_mais_barato.drop_duplicates("Item ID", keep="first")
+
+print("Produto mais caro:")
+print(df2)
+print("Produto mais barato:")
+print(df3)
